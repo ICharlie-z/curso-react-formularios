c2495147435cc0357d1dfad079496ae6582f22c7
Carlos Ron <carlos.ron.mouat@gmail.com>
2023-05-16 12:59:42 -0400

    localStorage todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
 import Todos from "./components/Todos";
 
-const initialTodos = [
-  {
-    id: 1,
-    title: 'Todo #01',
-    description: 'Descripción #01',
-    state: true,
-    priority: true
-  },
-  {
-    id: 2,
-    title: 'Todo #02',
-    description: 'Descripción #02',
-    state: false,
-    priority: false
-  },
-  {
-    id: 3,
-    title: 'Todo #03',
-    description: 'Descripción #03',
-    state: false,
-    priority: true
-  }
-];
+const initialTodos = (localStorage.getItem('todos'))? JSON.parse(localStorage.getItem('todos')) : [];
 
 const App = () => {
   const [todos, setTodos] = useState(initialTodos)
-
+  useEffect(() => { 
+    localStorage.setItem('todos', JSON.stringify(todos)
+  )}, [todos])
   const addTodo = (todo)=>{
     setTodos([...todos,todo]);
   }
